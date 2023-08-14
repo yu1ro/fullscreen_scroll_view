f6f9f846c371d7db80a11d792555811ef5fba000
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2023-08-14 22:24:00 +0900

    test code

--- test/fullscreen_scroll_view_test.dart
+++ test/fullscreen_scroll_view_test.dart
@@ -1,7 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:fullscreen_scroll_view/fullscreen_scroll_view.dart';
 
 void main() {
+  // This test is expected to fail
+  testWidgets('Expanded cannot be inside SingleChildScrollView', skip: true,
+      (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('data'),
+                Expanded(child: SizedBox.shrink()),
+                Text('data2'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  });
 
+  testWidgets('Expanded can be inside FullScreenScrollView', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: FullScreenScrollView(
+            child: Column(
+              children: [
+                Text('text'),
+                Expanded(child: SizedBox.shrink()),
+                Text('text2'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  });
 }
